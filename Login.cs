50afa365598da309a1af5b212dd04b289c4f7985
Nícolas Fernandes <nicolas.safer@outlook.com>
2023-03-13 22:58:36 -0300

    Login feito

--- Login.cs
+++ Login.cs
@@ -1,7 +1,16 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+using WindowsFormsApp3;
+
 namespace SistemaFrotaV3
 {
     public partial class Login : Form
     {
+        public conexao con = new conexao();
+
+        public string id_on;
+
+        Thread f1;
         public Login()
         {
             InitializeComponent();
@@ -40,6 +49,51 @@ namespace SistemaFrotaV3
         private void label4_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = con.getconexao();
+            conexao.Open();
+            string sql = "select id_usuario, nome_usuario,senha_usuario from tb_usuario where cnpj = @nome_usuario";
+            MySqlCommand comando_1 = new MySqlCommand(sql, conexao);
+            comando_1.Parameters.Clear();
+            comando_1.Parameters.AddWithValue("@nome_usuario", text_login.Text);
+
+            // comando_1.Parameters.Add("@senha_funcionario", MySqlDbType.Int32).Value = txtsenha_adm.Text;
+            comando_1.CommandType = CommandType.Text;
+            MySqlDataReader dr;
+            dr = comando_1.ExecuteReader();
+            dr.Read();
+
+            try
+            {
+                if (dr.GetString(2) == text_senha.Text)
+                {
+                    id_on = dr.GetString(0);
+                    MessageBox.Show("Foi");
+                    //// MessageBox.Show(id_on);
+                    this.Close();
+                    f1 = new Thread(AbrirJan);
+                    f1.SetApartmentState(ApartmentState.STA);
+                    f1.Start(id_on);
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou Senha incorreto");
+                }
+            }
+            catch (Exception ex)//caso de erro
+            {
+                MessageBox.Show("erro sistema", ex.Message);
+            }
+            conexao.Close();
+        }
+        private void AbrirJan(object obj)
+        {/*id_on*/
+            Application.Run(new Form1());
         }
     }
 }
